d2de58d3758d1a6d627fc3ac669018611d7d6963
bsparrow49 <aashishchaudhari249@gmail.com>
2026-01-07 20:15:07 +0545

    Initial commit: beginning of tracker.cpp, expense function completed

---
+++ expense-tracker/tracker.cpp
@@ -0,0 +1,90 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<vector>
+using namespace std;
+class Expense{
+    public:
+    string date;
+    string category;
+    string description;
+    double amount;
+    Expense(string date,string category,string description,double amount)
+    {
+        this->date=date;
+        this->category=category;
+        this->description=(description.empty()? "null":description);
+        this->amount=amount;
+    }
+    void save(const string& expense_file) const{
+        ofstream expfile(expense_file,ios::app);
+        if(expfile)
+        {
+            expfile<<date<<"\t\t\t\t\t\t"<<category<<"\t\t\t\t\t"<<amount<<"\t\t\t\t\t"<<description<<"\t\t\t\t\t"<<endl;
+        }
+        else
+        {
+            cout<<"Error: Could not open file."<<endl;
+        }
+        expfile.close();
+    }
+};
+void add_expense()
+{
+    string date,description;
+    double amount;
+    int category_choice;
+    vector<string>categories={"Food & Grocery","Travel","Rent","Electricity","Stationery","Shopping","Lunch","Others","Miscategory"};
+    cout<<"Enter date (DD MMM YYYY)";// 07 Jan 2026
+    cin>>date;
+    cout<<"\n----------Categories--------"<<endl;
+    for(size_t i=0;i<categories.size();i++)
+    {
+        cout<<i+1<<". "<<categories[i]<<endl;
+    }
+    cout<<"\nSelect Category:";
+    cin>>category_choice;
+    if(category_choice<1 || category_choice>categories.size())
+    {
+        cout<<"Invalid choice! Defaulting to Miscategory"<<endl;
+        category_choice=categories.size();
+    }
+    cout<<"Enter amount:";
+    cin>>amount;
+    cin.ignore();
+    cout<<"Enter description(Optional): ";
+    getline(cin,description);
+    Expense exp(date,categories[category_choice-1],description,amount);
+    exp.save("expense_file.txt");//saves using class method
+
+}
+void add_inflow();
+void view_expenses();
+void view_flow_summary();
+void save_exit();
+void main_menu_bar()
+{
+    cout<<"---------------MENU-------------"<<endl;
+    cout<<"1.Add Expense\n2.Add Inflow\n3.View Expenses\n4.View Flow Summary\n5.Save & Exit"<<endl;
+    int menu_choice;
+    cout<<"Enter Operation:";
+    cin>>menu_choice;
+    switch(menu_choice)
+    {
+        case 1:
+        add_expense();
+        case 2:
+        add_inflow();
+        case 3:
+        view_expenses();
+        case 4:
+        view_flow_summary();
+        case 5:
+        save_exit();
+    }
+
+}
+int main()
+{
+    
+}
